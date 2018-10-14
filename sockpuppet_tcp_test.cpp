2b161273dc368b4e65b4cf098df3567d9b7fa8b1
Marco Porsch <marco.porsch@posteo.de>
2018-10-14 18:30:06 +0200

    sockpuppet_tcp_test: have multiple clients connect to server and spawn individual server handlers

--- sockpuppet_tcp_test.cpp
+++ sockpuppet_tcp_test.cpp
@@ -1,68 +1,104 @@
 #include "socket.h"
 
+#ifdef HAVE_HELPER
+# include "../helper/print_unmangled.h"
+
+# define COUT PrintUnmangled()
+# define CERR PrintUnmangled(std::cerr)
+#else
+# define COUT std::cout
+# define CERR std::cerr
+#endif // HAVE_HELPER
+
 #include <cstdlib> // for EXIT_SUCCESS
 #include <iostream> // for std::cerr
 #include <string> // for std::string
 #include <thread> // for std::thread
 
+static int const clientCount = 3;
 bool success = true;
 
-void Server()
+void ServerHandler(SocketTcpClient client, SocketAddress clientAddr)
 try {
-  SocketAddress serverAddr("localhost:8554");
-  SocketTcpServer server(serverAddr);
-
-  auto t = server.Listen();
-  auto &&client = std::get<0>(t);
-  auto &&clientAddr = std::get<1>(t);
-
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  std::cout << "transmitting from "
-    << std::to_string(serverAddr) << " to "
+  COUT << "server transmitting to "
     << std::to_string(clientAddr) << std::endl;
 
   static char const hello[] = "hello";
   client.Transmit(hello, sizeof(hello));
 } catch (std::exception const &e) {
-  std::cerr << e.what() << std::endl;
+  CERR << e.what() << std::endl;
   success = false;
 }
 
-void Client()
+void Server(SocketAddress serverAddr)
 try {
-  SocketAddress serverAddr("localhost:8554");
-  SocketTcpClient client(serverAddr);
+  SocketTcpServer server(serverAddr);
 
-  std::cout << "receiving from "
+  COUT << "server listening at "
     << std::to_string(serverAddr) << std::endl;
 
-  char buffer[256];
-  for(int i = 0; i < 4; ++i) {
-    auto const received = client.Receive(buffer, sizeof(buffer));
-    if(received > 0U
-    && std::string(buffer, received).find("hello") != std::string::npos) {
-      return;
+  std::thread serverHandlers[clientCount];
+  for(auto &&serverHandler : serverHandlers) {
+    auto t = server.Listen();
+    auto &&client = std::get<0>(t);
+    auto &&clientAddr = std::get<1>(t);
+
+    serverHandler = std::thread(ServerHandler,
+                                std::move(client),
+                                std::move(clientAddr));
+  }
+
+  for(auto &&serverHandler : serverHandlers) {
+    if(serverHandler.joinable()) {
+      serverHandler.join();
     }
+  }
+} catch (std::exception const &e) {
+  CERR << e.what() << std::endl;
+  success = false;
+}
+
+void Client(SocketAddress serverAddr)
+try {
+  SocketTcpClient client(serverAddr);
+
+  COUT << "client " << std::this_thread::get_id()
+    << " connected to " << std::to_string(serverAddr)
+    << std::endl;
+
+  char buffer[256];
+  auto const received = client.Receive(buffer, sizeof(buffer));
+  if(received > 0U
+  && std::string(buffer, received).find("hello") != std::string::npos) {
+    return;
   }
 
   throw std::runtime_error("failed to receive hello");
 } catch (std::exception const &e) {
-  std::cerr << e.what() << std::endl;
+  CERR << e.what() << std::endl;
   success = false;
 }
 
 int main(int, char **)
 {
-  std::thread server(Server);
+  std::thread server(Server, SocketAddress("localhost:8554"));
+
   std::this_thread::sleep_for(std::chrono::seconds(1));
-  std::thread client(Client);
+
+  std::thread clients[clientCount];
+  for(auto &&client : clients) {
+    client = std::thread (Client, SocketAddress("localhost:8554"));
+  }
 
   if(server.joinable()) {
     server.join();
   }
-  if(client.joinable()) {
-    client.join();
+  for(auto &&client : clients) {
+    if(client.joinable()) {
+      client.join();
+    }
   }
 
   return (success ? EXIT_SUCCESS : EXIT_FAILURE);
