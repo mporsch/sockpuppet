6642e540fc5ac5b345af12d127098375177e157e
Marco Porsch <marco.porsch@posteo.de>
2018-11-17 11:53:36 +0100

    rewrite SocketAsync <-> SocketDriver interaction

--- src/socket_async_priv.cpp
+++ src/socket_async_priv.cpp
@@ -23,42 +23,28 @@ SocketDriver::SocketDriverPriv::~SocketDriverPriv()
 
 void SocketDriver::SocketDriverPriv::Step()
 {
-  decltype(sockets) socks;
-  {
-    std::lock_guard<std::mutex> lock(socketsMtx);
-
-    socks = sockets;
-  }
-
-  SOCKET fdMax = -1;
-  fd_set rfds;
-  fd_set wfds;
-  FD_ZERO(&rfds);
-  FD_ZERO(&wfds);
-  for(auto &&sock : socks) {
-    sock.get().AsyncFillFdSet(fdMax, rfds, wfds);
-  }
-
-  FD_SET(pipeTo.fd, &rfds);
-  fdMax = std::max(fdMax, pipeTo.fd);
+  auto t = PrepareFds();
+  auto &&fdMax = std::get<0>(t);
+  auto &&rfds = std::get<1>(t);
+  auto &&wfds = std::get<2>(t);
 
   if(auto const result = ::select(static_cast<int>(fdMax + 1), &rfds, &wfds, nullptr, nullptr)) {
     if(result < 0) {
       throw std::runtime_error("select failed: "
                                + std::string(std::strerror(errno)));
-    } else {
-      // readable/writable
-      if(FD_ISSET(pipeTo.fd, &rfds)) {
-        char dump[256U];
-        (void)pipeTo.Receive(dump, sizeof(dump), Socket::Time(0));
-      } else {
-        for(auto &&sock : socks) {
-          sock.get().AsyncCheckFdSet(rfds, wfds);
-        }
-      }
     }
   } else {
     throw std::runtime_error("select timed out");
+  }
+
+  // one or more sockets is readable/writable
+  if(FD_ISSET(pipeTo.fd, &rfds)) {
+    // a readable signalling socket triggers re-evaluating the sockets
+    Unbump();
+  } else if(auto task = CollectFdTask(rfds, wfds)) {
+    // because any of the user tasks may unregister/destroy a socket,
+    // the safe approach is to handle only one task at a time
+    task();
   }
 }
 
@@ -80,7 +66,6 @@ void SocketDriver::SocketDriverPriv::Register(
     SocketAsync::SocketAsyncPriv &sock)
 {
   std::lock_guard<std::mutex> lock(socketsMtx);
-
   sockets.emplace_back(sock);
 }
 
@@ -90,16 +75,15 @@ void SocketDriver::SocketDriverPriv::Unregister(
   {
     std::lock_guard<std::mutex> lock(socketsMtx);
 
-    auto cmp = [&](std::reference_wrapper<SocketAsync::SocketAsyncPriv> s) -> bool
-    {
-      return (&s.get() == &sock);
-    };
-
     sockets.erase(
-          std::remove_if(std::begin(sockets),
-                         std::end(sockets),
-                         cmp),
-          std::end(sockets));
+      std::remove_if(
+        std::begin(sockets),
+        std::end(sockets),
+        [&](SocketRef s) -> bool
+        {
+          return (&s.get() == &sock);
+        }),
+      std::end(sockets));
   }
 
   Bump();
@@ -111,11 +95,57 @@ void SocketDriver::SocketDriverPriv::Bump()
   pipeFrom.SendTo(&one, sizeof(one), pipeToAddr.SockAddrUdp());
 }
 
+void SocketDriver::SocketDriverPriv::Unbump()
+{
+  char dump[256U];
+  (void)pipeTo.Receive(dump, sizeof(dump), Socket::Time(0));
+}
+
+std::tuple<SOCKET, fd_set, fd_set> SocketDriver::SocketDriverPriv::PrepareFds()
+{
+  std::lock_guard<std::mutex> lock(socketsMtx);
+
+  SOCKET fdMax = -1;
+  fd_set rfds;
+  fd_set wfds;
+  FD_ZERO(&rfds);
+  FD_ZERO(&wfds);
+
+  // have the sockets set their file descriptors
+  for(auto &&sock : sockets) {
+    sock.get().DriverPrepareFds(fdMax, rfds, wfds);
+  }
+
+  // set the signalling socket file descriptor
+  FD_SET(pipeTo.fd, &rfds);
+  fdMax = std::max(fdMax, pipeTo.fd);
+
+  return std::tuple<SOCKET, fd_set, fd_set>{
+    fdMax
+  , rfds
+  , wfds
+  };
+}
+
+std::function<void()> SocketDriver::SocketDriverPriv::CollectFdTask(
+  fd_set const &rfds, fd_set const &wfds)
+{
+  std::lock_guard<std::mutex> lock(socketsMtx);
+
+  for(auto &&sock : sockets) {
+    if(auto const task = sock.get().DriverCollectFdTask(rfds, wfds)) {
+      return task;
+    }
+  }
+
+  return nullptr;
+}
+
 
 SocketAsync::SocketAsyncPriv::SocketAsyncPriv(
-    Socket::SocketPriv &&sock,
+    SocketPriv &&sock,
     std::shared_ptr<SocketDriver::SocketDriverPriv> &driver,
-    SocketAsync::Handlers handlers)
+    Handlers handlers)
   : SocketAsyncPriv(SocketBufferedPriv(std::move(sock), 0U, 0U),
                     driver,
                     handlers)
@@ -123,10 +153,10 @@ SocketAsync::SocketAsyncPriv::SocketAsyncPriv(
 }
 
 SocketAsync::SocketAsyncPriv::SocketAsyncPriv(
-    SocketBuffered::SocketBufferedPriv &&buff,
+    SocketBufferedPriv &&buff,
     std::shared_ptr<SocketDriver::SocketDriverPriv> &driver,
-    SocketAsync::Handlers handlers)
-  : SocketBuffered::SocketBufferedPriv(std::move(buff))
+    Handlers handlers)
+  : SocketBufferedPriv(std::move(buff))
   , driver(driver)
   , handlers(handlers)
 {
@@ -143,49 +173,19 @@ SocketAsync::SocketAsyncPriv::~SocketAsyncPriv()
 std::future<void> SocketAsync::SocketAsyncPriv::Send(
   SocketBufferPtr &&buffer)
 {
-  std::promise<void> promise;
-  auto ret = promise.get_future();
-
-  {
-    std::lock_guard<std::mutex> lock(sendQMtx);
-
-    sendQ.emplace(SendQElement{
-      std::move(promise)
-    , std::move(buffer)
-    });
-  }
-
-  if(auto const ptr = driver.lock()) {
-    ptr->Bump();
-  }
-
-  return ret;
+  return DoSend(sendQ,
+                std::move(buffer));
 }
 
 std::future<void> SocketAsync::SocketAsyncPriv::SendTo(
   SocketBufferPtr &&buffer, SockAddr const &dstAddr)
 {
-  std::promise<void> promise;
-  auto ret = promise.get_future();
-
-  {
-    std::lock_guard<std::mutex> lock(sendQMtx);
-
-    sendToQ.emplace(SendToQElement{
-      std::move(promise)
-    , std::move(buffer)
-    , dstAddr
-    });
-  }
-
-  if(auto const ptr = driver.lock()) {
-    ptr->Bump();
-  }
-
-  return ret;
+  return DoSend(sendToQ,
+                std::move(buffer),
+                dstAddr);
 }
 
-void SocketAsync::SocketAsyncPriv::AsyncFillFdSet(SOCKET &fdMax,
+void SocketAsync::SocketAsyncPriv::DriverPrepareFds(SOCKET &fdMax,
   fd_set &rfds, fd_set &wfds)
 {
   if(handlers.connect || handlers.receive || handlers.receiveFrom) {
@@ -203,54 +203,61 @@ void SocketAsync::SocketAsyncPriv::AsyncFillFdSet(SOCKET &fdMax,
   }
 }
 
-void SocketAsync::SocketAsyncPriv::AsyncCheckFdSet(fd_set const &rfds,
-  fd_set const &wfds)
+std::function<void()> SocketAsync::SocketAsyncPriv::DriverCollectFdTask(
+  fd_set const &rfds, fd_set const &wfds)
 {
-  try {
-    if(FD_ISSET(this->fd, &rfds)) {
-      if(handlers.connect) {
-        auto t = this->Accept(Socket::Time(0));
+  if(FD_ISSET(this->fd, &rfds)) {
+    return std::bind(&SocketAsyncPriv::DriverHandleReadable, this);
+  } else if(FD_ISSET(this->fd, &wfds)) {
+    return std::bind(&SocketAsyncPriv::DriverHandleWritable, this);
+  } else {
+    return nullptr;
+  }
+}
 
-        handlers.connect(
-              std::tuple<SocketTcpClient, SocketAddress>{
-                SocketTcpClient(std::move(std::get<0>(t)))
-              , SocketAddress(std::move(std::get<1>(t)))
-              });
+void SocketAsync::SocketAsyncPriv::DriverHandleReadable()
+try {
+  if(handlers.connect) {
+    auto t = this->Accept(Time(0));
+    this->Listen();
 
-        this->Listen();
-      } else if(handlers.receive) {
-        handlers.receive(this->Receive(Socket::Time(0)));
-      } else if(handlers.receiveFrom) {
-        handlers.receiveFrom(this->ReceiveFrom(Socket::Time(0)));
-      }
-    }
+    handlers.connect(
+      std::tuple<SocketTcpClient, SocketAddress>{
+        SocketTcpClient(std::move(std::get<0>(t)))
+      , SocketAddress(std::move(std::get<1>(t)))
+      });
+  } else if(handlers.receive) {
+    handlers.receive(this->Receive(Time(0)));
+  } else if(handlers.receiveFrom) {
+    handlers.receiveFrom(this->ReceiveFrom(Time(0)));
+  } else {
+    throw std::logic_error("no read handler");
+  }
+} catch(std::runtime_error const &) {
+  if(handlers.disconnect) {
+    handlers.disconnect(nullptr); // TODO
+  }
+}
 
-    if(FD_ISSET(this->fd, &wfds)) {
-      std::unique_lock<std::mutex> lock(sendQMtx);
+void SocketAsync::SocketAsyncPriv::DriverHandleWritable()
+{
+  std::unique_lock<std::mutex> lock(sendQMtx);
 
-      if(!sendQ.empty()) {
-        auto e = std::move(sendQ.front());
-        sendQ.pop();
-        lock.unlock();
+  if(!sendQ.empty()) {
+    auto e = std::move(sendQ.front());
+    sendQ.pop();
+    lock.unlock();
 
-        SocketPriv::Send(e.buffer->data(), e.buffer->size(), Socket::Time(0));
-        e.promise.set_value();
-      } else if(!sendToQ.empty()) {
-        auto e = std::move(sendToQ.front());
-        sendToQ.pop();
-        lock.unlock();
+    SocketPriv::Send(e.buffer->data(), e.buffer->size(), Time(0));
+    e.promise.set_value();
+  } else if(!sendToQ.empty()) {
+    auto e = std::move(sendToQ.front());
+    sendToQ.pop();
+    lock.unlock();
 
-        SocketPriv::SendTo(e.buffer->data(), e.buffer->size(), e.addr);
-        e.promise.set_value();
-      }
-    }
-  } catch(std::exception const &) {
-    if(auto const ptr = driver.lock()) {
-      ptr->Unregister(*this);
-    }
-
-    if(handlers.disconnect) {
-      handlers.disconnect(nullptr); // TODO
-    }
+    SocketPriv::SendTo(e.buffer->data(), e.buffer->size(), e.addr);
+    e.promise.set_value();
+  } else {
+    throw std::logic_error("send buffer emptied unexpectedly");
   }
 }
--- src/socket_async_priv.h
+++ src/socket_async_priv.h
@@ -12,13 +12,16 @@
 using SOCKET = int;
 #endif // _WIN32
 
+#include <functional> // for std::function
 #include <future> // for std::future
 #include <mutex> // for std::mutex
 #include <queue> // for std::queue
 
 struct SocketDriver::SocketDriverPriv
 {
-  std::vector<std::reference_wrapper<SocketAsync::SocketAsyncPriv>> sockets;
+  using SocketRef = std::reference_wrapper<SocketAsync::SocketAsyncPriv>;
+
+  std::vector<SocketRef> sockets;
   std::mutex socketsMtx;
   SocketAddressAddrinfo pipeToAddr;
   Socket::SocketPriv pipeFrom;
@@ -37,6 +40,10 @@ struct SocketDriver::SocketDriverPriv
   void Unregister(SocketAsync::SocketAsyncPriv &sock);
 
   void Bump();
+  void Unbump();
+
+  std::tuple<SOCKET, fd_set, fd_set> PrepareFds();
+  std::function<void()> CollectFdTask(fd_set const &rfds, fd_set const &wfds);
 };
 
 struct SocketAsync::SocketAsyncPriv : public SocketBuffered::SocketBufferedPriv
@@ -44,44 +51,69 @@ struct SocketAsync::SocketAsyncPriv : public SocketBuffered::SocketBufferedPriv
   struct SendQElement
   {
     std::promise<void> promise;
-    SocketBuffered::SocketBufferPtr buffer;
+    SocketBufferPtr buffer;
   };
   using SendQ = std::queue<SendQElement>;
 
   struct SendToQElement
   {
     std::promise<void> promise;
-    SocketBuffered::SocketBufferPtr buffer;
+    SocketBufferPtr buffer;
     SockAddr addr;
   };
   using SendToQ = std::queue<SendToQElement>;
 
   std::weak_ptr<SocketDriver::SocketDriverPriv> driver;
-  SocketAsync::Handlers handlers;
+  Handlers handlers;
   SendQ sendQ;
   SendToQ sendToQ;
   std::mutex sendQMtx;
 
-  SocketAsyncPriv(Socket::SocketPriv &&sock,
+  SocketAsyncPriv(SocketPriv &&sock,
                   std::shared_ptr<SocketDriver::SocketDriverPriv> &driver,
-                  SocketAsync::Handlers handlers);
-  SocketAsyncPriv(SocketBuffered::SocketBufferedPriv &&buff,
+                  Handlers handlers);
+  SocketAsyncPriv(SocketBufferedPriv &&buff,
                   std::shared_ptr<SocketDriver::SocketDriverPriv> &driver,
-                  SocketAsync::Handlers handlers);
+                  Handlers handlers);
   virtual ~SocketAsyncPriv();
 
-  std::future<void> Send(SocketBuffered::SocketBufferPtr &&buffer);
+  std::future<void> Send(SocketBufferPtr &&buffer);
 
-  std::future<void> SendTo(SocketBuffered::SocketBufferPtr &&buffer,
+  std::future<void> SendTo(SocketBufferPtr &&buffer,
                            SockAddr const &dstAddr);
 
-  void AsyncFillFdSet(SOCKET &fdMax,
-                      fd_set &rfds,
-                      fd_set &wfds);
-  void AsyncCheckFdSet(fd_set const &rfds,
-                       fd_set const &wfds);
-  void AsyncSend();
-  void AsyncReceive();
+  template<typename QueueElement, typename... Args>
+  std::future<void> DoSend(std::queue<QueueElement> &q,
+                           Args&&... args)
+  {
+    std::promise<void> promise;
+    auto ret = promise.get_future();
+
+    {
+      std::lock_guard<std::mutex> lock(sendQMtx);
+
+      q.emplace(
+        QueueElement{
+          std::move(promise)
+        , std::forward<Args>(args)...
+        });
+    }
+
+    if(auto const ptr = driver.lock()) {
+      ptr->Bump();
+    }
+
+    return ret;
+  }
+
+  void DriverPrepareFds(SOCKET &fdMax,
+                        fd_set &rfds,
+                        fd_set &wfds);
+
+  std::function<void()> DriverCollectFdTask(fd_set const &rfds,
+                                            fd_set const &wfds);
+  void DriverHandleReadable();
+  void DriverHandleWritable();
 };
 
 #endif // SOCKET_ASYNC_PRIV
