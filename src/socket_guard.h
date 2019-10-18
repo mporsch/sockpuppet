c3b42abc7f9a676cef80f0a6eca5d696ef254bce
Marco Porsch <marco.porsch@posteo.de>
2019-10-18 19:26:20 +0200

    SocketGuard: simplify unix<->windows distinction

--- src/socket_guard.h
+++ src/socket_guard.h
@@ -3,15 +3,16 @@
 
 namespace sockpuppet {
 
-class SocketGuard
+struct SocketGuard
 {
-public:
+#ifdef _WIN32
   SocketGuard();
   SocketGuard(SocketGuard const &other) = delete;
   SocketGuard(SocketGuard &&other) = delete;
   ~SocketGuard();
   SocketGuard &operator=(SocketGuard const &other) = delete;
   SocketGuard &operator=(SocketGuard &&other) = delete;
+#endif // _WIN32
 };
 
 } // namespace sockpuppet
