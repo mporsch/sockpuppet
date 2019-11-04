288aacf9ae8c9d21aea713a069f8f6c69cbaaee8
Marco Porsch <marco.porsch@posteo.de>
2019-11-04 10:22:18 +0100

    sockpuppet_tcp_*: improve documentation

--- examples/sockpuppet_tcp_client.cpp
+++ examples/sockpuppet_tcp_client.cpp
@@ -6,6 +6,49 @@
 
 using namespace sockpuppet;
 
+void Client(Address remoteAddress)
+{
+  // connect a TCP client socket to given address
+  SocketTcpClient client(remoteAddress);
+
+  // print the bound TCP socket address
+  // (has OS-assigned interface and port number)
+  // and remote address
+  std::cout << "established connection "
+            << to_string(client.LocalAddress())
+            << " -> "
+            << to_string(remoteAddress)
+            << std::endl;
+
+  // query and send until cancelled
+  for(;;) {
+    // query a string to send from the command line
+    std::string line;
+    std::cout << "message to send? (empty for exit) - ";
+    std::getline(std::cin, line);
+
+    if(line.empty()) {
+      break;
+    } else {
+      static Socket::Duration const noTimeout(-1);
+
+      // send the given string data to the connected peer
+      // negative timeout -> blocking until sent
+      // ignore the return value as - with unlimited timeout -
+      // it will always match the sent size
+      (void)client.Send(line.c_str(),
+                        line.size(),
+                        noTimeout);
+    }
+  }
+
+  std::cout << "closing connection "
+            << to_string(client.LocalAddress())
+            << " -> "
+            << to_string(remoteAddress)
+            << std::endl;
+}
+
 int main(int argc, char *argv[])
 try {
   if(argc < 2) {
@@ -15,25 +58,11 @@ try {
          "e.g. \"localhost:8554\""
       << std::endl;
   } else {
-    Address const serverAddr(argv[1]);
-    SocketTcpClient client(serverAddr);
+    // parse given address string
+    Address remoteAddress(argv[1]);
 
-    std::cout << "connected to "
-      << to_string(serverAddr) << std::endl;
-
-    for(;;) {
-      std::string line;
-      std::cout << "message to send? (empty for exit) - ";
-      std::getline(std::cin, line);
-      if(line.empty()) {
-        break;
-      } else {
-        (void)client.Send(line.c_str(), line.size());
-      }
-    }
-
-    std::cout << "closing connection to "
-      << to_string(serverAddr) << std::endl;
+    // create, connect and run a TCP client socket
+    Client(remoteAddress);
   }
 
   return EXIT_SUCCESS;
--- examples/sockpuppet_tcp_server.cpp
+++ examples/sockpuppet_tcp_server.cpp
@@ -11,16 +11,52 @@ try {
   auto &&clientSock = p.first;
   auto &&clientAddr = p.second;
 
-  std::cout << "client " << to_string(clientAddr)
-    << " connected" << std::endl;
+  std::cout << "connection "
+            << to_string(clientAddr)
+            << " <- "
+            << to_string(clientSock.LocalAddress())
+            << " accepted"
+            << std::endl;
 
+  // receive and print until Ctrl-C or client disconnect
   for(;;) {
     char buffer[256];
-    auto const received = clientSock.Receive(buffer, sizeof(buffer));
+    static Socket::Duration const noTimeout(-1);
+
+    // wait for and receive incoming data into provided buffer
+    // negative timeout -> blocking until receipt
+    size_t received = clientSock.Receive(buffer,
+                                         sizeof(buffer),
+                                         noTimeout);
+
+    // print whatever has just been received
     std::cout << std::string(buffer, received) << std::endl;
   }
 } catch (std::exception const &e) {
+  // (most probably) client disconnected
   std::cerr << e.what() << std::endl;
+}
+
+void Server(Address bindAddress)
+{
+  // bind a TCP server socket to given address
+  SocketTcpServer server(bindAddress);
+
+  // listen for and accept incoming connections until Ctrl-C
+  for(;;) {
+    static Socket::Duration const noTimeout(-1);
+
+    // print the bound TCP socket address
+    // (might have OS-assigned port number if
+    // it has not been explicitly set in the bind address)
+    std::cout << "listening at "
+              << to_string(server.LocalAddress())
+              << std::endl;
+
+    // wait for and accept incoming connections
+    // negative timeout -> blocking until connection
+    ServerHandler(server.Listen(noTimeout));
+  }
 }
 
 int main(int argc, char *argv[])
@@ -32,15 +68,11 @@ try {
          "e.g. \"localhost:8554\""
       << std::endl;
   } else {
-    Address const serverAddr(argv[1]);
-    SocketTcpServer server(serverAddr);
+    // parse given address string
+    Address bindAddress(argv[1]);
 
-    for(;;) {
-      std::cout << "listening at " << to_string(server.LocalAddress())
-                << std::endl;
-
-      ServerHandler(server.Listen());
-    }
+    // create and run a TCP server socket
+    Server(bindAddress);
   }
 
   return EXIT_SUCCESS;
