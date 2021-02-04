515676d2997e2c416678be67eaf28b8dfbf78b22
Marco Porsch <marco.porsch@posteo.de>
2021-02-04 20:43:23 +0100

    fix seemingly late shutdown timing in todo test, add comments

--- test/sockpuppet_todo_test.cpp
+++ test/sockpuppet_todo_test.cpp
@@ -7,12 +7,15 @@
 
 using namespace sockpuppet;
 
-static auto startTime = Clock::now();
 static SocketDriver driver;
+static auto startTime = Clock::now();
 
 std::ostream &operator<<(std::ostream &os, TimePoint tp)
 {
-  os << std::setw(4) << (tp - startTime).count() / 1000000 << "ms";
+  using namespace std::chrono;
+
+  auto msecSinceStart = duration_cast<milliseconds>(tp - startTime);
+  os << std::setw(4) << msecSinceStart.count() << "ms";
   return os;
 }
 
@@ -26,30 +29,39 @@ void ScheduledPrint(char const *what, TimePoint expected)
 
 struct Repeatable
 {
-  ToDo todo;
-  Duration period;
+  ToDo todo; // ToDo is stored for access with Shift/Cancel
+  Duration interval;
   TimePoint next;
 
-  Repeatable(Duration period, TimePoint until)
-    : todo(driver, std::bind(&Repeatable::OnTime, this), period)
-    , period(period)
-    , next(Clock::now() + period)
+  Repeatable(Duration interval, TimePoint until)
+    : todo(driver,
+           std::bind(&Repeatable::OnTime, this),
+           interval) // schedule first execution
+    , interval(interval)
+    , next(Clock::now() + interval)
   {
-    ToDo(driver, std::bind(&Repeatable::Cancel, this), until);
+    // schedule own ToDo cancellation
+    ToDo(driver,
+         std::bind(&Repeatable::Cancel, this),
+         until);
   }
 
   void OnTime()
   {
     auto now = next;
-    next += period;
+    next += interval;
 
-    todo.Shift(period);
+    // reschedule to run again at next interval
+    todo.Shift(interval);
 
     ScheduledPrint("repeating", now);
   }
 
   void Cancel()
   {
+    // everything is running in one thread;
+    // there is no danger of race conditions
+    // between OnTime and Cancel
     todo.Cancel();
   }
 };
@@ -64,23 +76,37 @@ int main(int, char **)
 try {
   auto now = Clock::now();
 
-  // schedule-and-forget task
-  ToDo(driver, std::bind(ScheduledPrint, "once", now + Duration(50)), Duration(50));
+  // schedule-and-forget task; no need to store the created object
+  (void)ToDo(driver,
+             std::bind(ScheduledPrint,
+                       "once",
+                       now + Duration(50)),
+             Duration(50));
 
   // rescheduling task that cancels itself eventually
-  Repeatable repeating(Duration(200), now + Duration(1500));
+  Repeatable repeating(Duration(200),
+                       now + Duration(1500));
 
-  // task that is scheduled conditionally
-  ToDo maybe(driver, std::bind(ScheduledPrint, "maybe", now + Duration(150)));
+  // task that is scheduled conditionally after
+  // being created unscheduled
+  ToDo maybe(driver,
+             std::bind(ScheduledPrint,
+                       "maybe",
+                       now + Duration(150)));
   if(true) {
     maybe.Shift(now + Duration(150));
   }
 
+  // schedule task to shut down eventually
+  ToDo(driver,
+       std::bind(Shutdown,
+                 now + Duration(2000)),
+       Duration(2000));
+
+  // use the different socket driver loop methods
   driver.Step(Duration(0));
   driver.Step(Duration(150));
   driver.Step(Duration(-1));
-
-  ToDo(driver, std::bind(Shutdown, now + Duration(2000)), Duration(2000));
   driver.Run();
 
   return EXIT_SUCCESS;
