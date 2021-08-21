05ca8f373459e41467e2589f8f4451d3daf9b8af
Marco Porsch <marco.porsch@posteo.de>
2021-08-21 10:34:55 +0200

    examples: cleanup and document chat print buffer

--- examples/sockpuppet_chat_io_print.h
+++ examples/sockpuppet_chat_io_print.h
@@ -19,9 +19,9 @@ static bool const isAnsiCapable = []() -> bool {
   DWORD flags = 0U;
   (void)GetConsoleMode(console, &flags);
   return (flags & ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-#else // _WIN32 // _WIN32
+#else // _WIN32
   return true;
-#endif // _WIN32 // _WIN32
+#endif // _WIN32
 }();
 
 struct ClearLine
@@ -31,22 +31,22 @@ struct ClearLine
 inline std::ostream &operator<<(std::ostream &os, ClearLine)
 {
   if(isAnsiCapable) {
-    os << '\r' // move cursor to the left
-       << "\x1B[K" // clear everything right of cursor
+    os << "\x1B[2K" // clear whole current line
        << std::flush;
   } else {
 #ifdef _WIN32
     auto console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // get coordinates
+    // get line start coordinates and width
     CONSOLE_SCREEN_BUFFER_INFO sbi;
     (void)GetConsoleScreenBufferInfo(console, &sbi);
-    COORD coord = sbi.dwCursorPosition;
+    COORD pos{0, sbi.dwCursorPosition.Y};
     auto width = static_cast<DWORD>(sbi.dwSize.X);
 
+    // clear with space character
     DWORD written;
-    (void)FillConsoleOutputCharacter(console, ' ', width, coord, &written);
-    (void)FillConsoleOutputAttribute(console, sbi.wAttributes, width, coord, &written);
+    (void)FillConsoleOutputCharacter(console, ' ', width, pos, &written);
+    (void)FillConsoleOutputAttribute(console, sbi.wAttributes, width, pos, &written);
 #endif // _WIN32
   }
   return os;
@@ -55,15 +55,14 @@ inline std::ostream &operator<<(std::ostream &os, ClearLine)
 static ClearLine const clearLine;
 
 
-struct CursorUp
+struct CursorUpFront
 {
 };
 
-inline std::ostream &operator<<(std::ostream &os, CursorUp)
+inline std::ostream &operator<<(std::ostream &os, CursorUpFront)
 {
   if(isAnsiCapable) {
-    os << "\x1B[1F" // move cursor up front
-       << "\x1B[K" // clear everything right of cursor
+    os << "\x1B[1F" // move cursor to front 1 line up
        << std::flush;
   } else {
 #ifdef _WIN32
@@ -72,79 +71,89 @@ inline std::ostream &operator<<(std::ostream &os, CursorUp)
     // get coordinates
     CONSOLE_SCREEN_BUFFER_INFO sbi;
     (void)GetConsoleScreenBufferInfo(console, &sbi);
-    COORD coord{0, static_cast<SHORT>(sbi.dwCursorPosition.Y - 1)};
-    auto width = static_cast<DWORD>(sbi.dwSize.X);
-
-    // clear line
-    DWORD written;
-    (void)FillConsoleOutputCharacter(console, ' ', width, coord, &written);
-    (void)FillConsoleOutputAttribute(console, sbi.wAttributes, width, coord, &written);
 
     // position cursor
-    (void)SetConsoleCursorPosition(console, coord);
+    COORD pos{0, static_cast<SHORT>(sbi.dwCursorPosition.Y - 1)};
+    (void)SetConsoleCursorPosition(console, pos);
 #endif // _WIN32
   }
   return os;
 }
 
-static CursorUp const cursorUp;
+static CursorUpFront const cursorUpFront;
+
+
+/// Moves cursor to front N lines up on instantiation and
+/// resets it on release. In the meantime lines can be
+/// printed to normally (advance using regular newlines).
+/// @note  It is not possible to insert new lines this way.
+/// @note  Line contents are not modified.
+struct CursorUpFrontGuard
+{
+  std::ostream &os;
+#ifdef _WIN32
+  HANDLE console;
+  COORD inputPos;
+#endif // _WIN32
+
+  CursorUpFrontGuard(std::ostream &os, size_t count)
+    : os(os)
+  {
+    if(isAnsiCapable) {
+      os << "\x1B[s" // save current cursor position
+         << "\x1B[" << count << "F" // move cursor to front N lines up
+         << std::flush;
+    } else {
+#ifdef _WIN32
+      console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+      // get coordinates
+      CONSOLE_SCREEN_BUFFER_INFO sbi;
+      (void)GetConsoleScreenBufferInfo(console, &sbi);
+      inputPos = sbi.dwCursorPosition;
+      COORD outputPos{0, static_cast<SHORT>(inputPos.Y - static_cast<SHORT>(count))};
+
+      // position cursor
+      (void)SetConsoleCursorPosition(console, outputPos);
+#endif // _WIN32
+    }
+  }
+
+  ~CursorUpFrontGuard()
+  {
+    if(isAnsiCapable) {
+      os << "\x1B[u" // restore saved cursor position
+         << std::flush;
+    } else {
+#ifdef _WIN32
+      (void)SetConsoleCursorPosition(console, inputPos);
+#endif // _WIN32
+    }
+  }
+};
 
 } // namespace io_print_detail
 
+/// Manages a given number of terminal lines to print a history
+/// of messages (e.g. external input) while user input can be entered below
+/// without being interrupted by asynchronous message prints.
+/// The buffer keeps the number of terminal lines constant and erases
+/// its own history in FIFO fashion.
+/// @note  Messages longer than a terminal line cause undefined behaviour.
 struct IOPrintBuffer : private std::vector<std::string>
 {
   std::ostream &os;
-
-  struct CursorUpGuard
-  {
-    std::ostream &os;
-#ifdef _WIN32
-    HANDLE console;
-    COORD inputCoord;
-#endif // _WIN32
-
-    CursorUpGuard(std::ostream &os, size_t length)
-      : os(os)
-    {
-      if(io_print_detail::isAnsiCapable) {
-        os << "\x1B[s" // save current cursor position
-           << "\x1B[" << length << "F" // move cursor up front
-           << std::flush;
-      } else {
-#ifdef _WIN32
-        console = GetStdHandle(STD_OUTPUT_HANDLE);
-
-        CONSOLE_SCREEN_BUFFER_INFO sbi;
-        (void)GetConsoleScreenBufferInfo(console, &sbi);
-
-        inputCoord = sbi.dwCursorPosition;
-
-        COORD outputCoord{0, static_cast<SHORT>(inputCoord.Y - length)};
-        (void)SetConsoleCursorPosition(console, outputCoord);
-#endif // _WIN32
-      }
-    }
-
-    ~CursorUpGuard()
-    {
-      if(io_print_detail::isAnsiCapable) {
-        os << "\x1B[u" << std::flush; // restore saved cursor position
-      } else {
-#ifdef _WIN32
-        (void)SetConsoleCursorPosition(console, inputCoord);
-#endif // _WIN32
-      }
-    }
-  };
 
   IOPrintBuffer(std::ostream &os, size_t length)
     : std::vector<std::string>(length)
     , os(os)
   {
-    for(size_t i = 0U; i < length + 1; ++i) {
+    for(size_t i = 0U; i <= length; ++i) {
       os << '\n';
     }
-    os << io_print_detail::cursorUp << std::flush;
+    os << io_print_detail::cursorUpFront
+       << io_print_detail::clearLine
+       << std::flush;
   }
 
   inline std::string Query(std::string const &prompt) const
@@ -152,9 +161,11 @@ struct IOPrintBuffer : private std::vector<std::string>
     os << prompt << std::flush;
 
     std::string line;
-    std::getline(std::cin, line);
+    (void)std::getline(std::cin, line);
 
-    os << io_print_detail::cursorUp << std::flush;
+    os << io_print_detail::cursorUpFront
+       << io_print_detail::clearLine
+       << std::flush;
 
     return line;
   }
@@ -169,7 +180,7 @@ struct IOPrintBuffer : private std::vector<std::string>
 
   inline void Print()
   {
-    CursorUpGuard sbg(os, this->size());
+    io_print_detail::CursorUpFrontGuard ufg(os, this->size());
     for(auto &&line : *this) {
       os << io_print_detail::clearLine << line << std::endl;
     }
