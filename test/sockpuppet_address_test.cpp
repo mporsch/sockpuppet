80e3d4d13e11881aa11daa7fccb19a2dcfb851d6
Marco Porsch <marco.porsch@posteo.de>
2021-08-27 01:04:08 +0200

    test: add URIs/hosts with path to address test

--- test/sockpuppet_address_test.cpp
+++ test/sockpuppet_address_test.cpp
@@ -21,23 +21,53 @@ void Test(Args&&... args)
 
 int main(int, char **)
 try {
+  // localhost IPv4, OS-assigned port number
   Test();
+
+  // localhost IPv4, fixed port number
   Test(554);
 
+  // localhost URI, OS-assigned port number
   Test("localhost");
+
+  // localhost URI with port/protocol
   Test("localhost:554");
   Test("http://localhost");
 
+  // localhost host with port/protocol
   Test("localhost", "554");
   Test("localhost", "http");
 
+  // IPv4 URI without port/protocol
+  Test("91.198.174.192");
+
+  // IPv4 URI with port/protocol
+  Test("http://91.198.174.192");
+  Test("91.198.174.192:80");
+
+  // IPv4 URI with port/protocol and path
+  Test("https://91.198.174.192/wiki/Wikipedia:Hauptseite");
+  Test("91.198.174.192:443/wiki/Wikipedia:Hauptseite");
+
+  // IPv4 host with port/protocol
+  Test("91.198.174.192", "http");
+  Test("91.198.174.192", "80");
+
+  // IPv6 URI without port/protocol
   Test("::1");
   Test("a:b::c:1");
+
+  // IPv6 URI with port/protocol
   Test("[::1]:554");
   Test("[a:b::c:1]:554");
   Test("http://::1");
   Test("http://a:b::c:1");
 
+  // IPv6 URI with port/protocol and path
+  Test("https://::/wiki/Wikipedia:Hauptseite");
+  Test("[::]:443/wiki/Wikipedia:Hauptseite");
+
+  // IPv6 host without port/protocol
   Test("::1", "554");
   Test("a:b::c:1", "554");
   Test("::1", "http");
