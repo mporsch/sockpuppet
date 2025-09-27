f3ff5dc4a6c15ba00b2835059bc2dd9758725dc2
Marco Porsch <marco.porsch@posteo.de>
2025-09-27 14:13:52 +0200

    Address: address index lookup (Windows-only)

--- src/address_impl_win.cpp
+++ src/address_impl_win.cpp
@@ -2,6 +2,10 @@
 
 #include "address_impl.h"
 
+#include <iphlpapi.h> // for GetAdaptersAddresses
+
+#include <stdexcept> // for std::runtime_error
+
 namespace sockpuppet {
 
 std::vector<Address>
@@ -23,6 +27,48 @@ Address::AddressImpl::LocalAddresses()
                        it->ai_addrlen));
   }
   return ret;
+}
+
+unsigned int Address::AddressImpl::LocalInterfaceIndex() const
+{
+  std::string buffer(1024, '\0');
+  for(int i = 0; i < 3; ++i) {
+    auto size = static_cast<ULONG>(buffer.size());
+    auto res = ::GetAdaptersAddresses(
+      AF_UNSPEC, // Family
+      0, // Flags,
+      nullptr, // Reserved
+      reinterpret_cast<PIP_ADAPTER_ADDRESSES>(buffer.data()), // AdapterAddresses
+      &size); // SizePointer
+    if(res == NO_ERROR) {
+      break;
+    } else if(res == ERROR_BUFFER_OVERFLOW) {
+      buffer.resize(size);
+    } else {
+      throw std::runtime_error("failed to get adapters info");
+    }
+  }
+
+  //auto const family = Family();
+  auto const thisHost = Host();
+  for(auto adapter = reinterpret_cast<IP_ADAPTER_ADDRESSES const *>(buffer.data()); adapter; adapter = adapter->Next) {
+    for(auto address = adapter->FirstUnicastAddress; address; address = address->Next) {
+      auto otherHost = SockAddrStorage(
+        reinterpret_cast<sockaddr const *>(address->Address.lpSockaddr),
+        static_cast<size_t>(address->Address.iSockaddrLength)
+      ).Host();
+
+      if(true &&
+          (address->Address.lpSockaddr->sa_family == AF_INET) &&
+          (otherHost == thisHost))
+        return adapter->IfIndex;
+      else if(true &&
+          (address->Address.lpSockaddr->sa_family == AF_INET6) &&
+          (otherHost == thisHost))
+        return adapter->Ipv6IfIndex;
+    }
+  }
+  throw std::runtime_error("failed to determine local address interface index");
 }
 
 } // namespace sockpuppet
